77cb7f50c3863027b5be5b52d4e2c01968ab59a5
Aditya-Raj16 <adityaaryan183@gmail.com>
2023-07-31 16:19:54 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,74 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+const mongoose = require("mongoose");
+const async = require("async");
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "A blog, short for weblog, is a frequently updated web page used for personal commentary or business content. Blogs are often interactive and include sections at the bottom of individual blog posts where readers can leave comments. Most are written in a conversational style to reflect the voice and personal views of the blogger. Some businesses use blogs to connect with target audiences and sell products. Blogs were originally called weblogs, which were websites that consisted of a series of entries arranged in reverse chronological order, so the newest posts appeared at the top. They were frequently updated with new information about various topics.";
+const contactContent = "Contact Us Directly on Twitter: @ARblog , Instagram: @ARblog , Gmail: arjul268@gmail.com ,Message us directly on 8656622207";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://127.0.0.1:27017/blogDB", {useNewUrlParser: true});
+
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", async(req, res) => {
+  const posts = await Post.find({});
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts: posts
+  });   
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = new Post ({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+
+  post.save();
+  res.redirect("/");
+});
+
+app.get("/posts/:postId", async(req, res)=> {
+
+  const requestedPostId = req.params.postId;
+  
+    const post = await Post.findOne({_id: requestedPostId}).exec();
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+  });
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server started on port 3000");
+});
