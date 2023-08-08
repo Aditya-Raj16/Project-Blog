ee9b11ab251eb3b34b9c54b0c07b50c3920af000
Aditya-Raj16 <adityarayan183@gmail.com>
2023-08-08 22:41:12 +0530

    connection to database

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const async = require("async");
+const colors = require("colors");
+const connectDb = require("./config/db");
+// config dot env file
+dotenv.config();
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "A blog, short for weblog, is a frequently updated web page used for personal commentary or business content. Blogs are often interactive and include sections at the bottom of individual blog posts where readers can leave comments. Most are written in a conversational style to reflect the voice and personal views of the blogger. Some businesses use blogs to connect with target audiences and sell products. Blogs were originally called weblogs, which were websites that consisted of a series of entries arranged in reverse chronological order, so the newest posts appeared at the top. They were frequently updated with new information about various topics.";
 const contactContent = "Contact Us Directly on Twitter: @ARblog , Instagram: @ARblog , Gmail: arjul268@gmail.com ,Message us directly on 8656622207";
 
+//databse call
+connectDb();
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -18,7 +25,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://127.0.0.1:27017/blogDB", {useNewUrlParser: true});
+//mongoose.connect("mongodb://127.0.0.1:27017/blogDB", {useNewUrlParser: true});
 
 
 const postSchema = {
